b3350debc95ea4a5eb8ebe86bfcfb5eb352312ba
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-01-10 12:41:47 +0100

    end exercise

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let num=0;
-let i=1;
+let num = 0;
+let i = 1;
 
 for (let i = 1; i <= 100; i++) {
-    num=num+1
-    if (num%3==0 ) {
-        if(num%5==0){
-            console.log("FizzBuzz")
-        } else{
-            console.log("Fizz")
-        }
-        
+  num = num + 1;
+  // controllo se il numero è divisibile per 3
+  if (num % 3 == 0) {
+    // controllo se il numero è divisibile per 5
+    if (num % 5 == 0) {
+      console.log("FizzBuzz");
+    } else {
+      console.log("Fizz");
     }
-        
-    
-    
+  } else if (num % 5 == 0) {
+    console.log("buzz");
+  } else {
+    console.log(num);
+  } 
 }
