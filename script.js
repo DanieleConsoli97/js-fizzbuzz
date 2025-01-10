ad1f4f6a51666b2f2ec9d2e5e0a5408e9774e346
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-01-10 12:33:17 +0100

    add first condition

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let numero=0;
+let num=0;
 let i=1;
 
-alert(12%3);
+for (let i = 1; i <= 100; i++) {
+    num=num+1
+    if (num%3==0 ) {
+        if(num%5==0){
+            console.log("FizzBuzz")
+        } else{
+            console.log("Fizz")
+        }
+        
+    }
+        
+    
+    
+}
